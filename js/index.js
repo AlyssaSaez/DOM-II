afb256a34178c61f3448ae4cfa5862ef0feec239
Alyssa Saez <xcthaeh@gmail.com>
2020-03-17 21:31:52 -0400

    Text appears when double clicking paragraph

--- js/index.js
+++ js/index.js
@@ -40,3 +40,11 @@ btn[1].addEventListener('mouseover', _ => {
 btn[2].addEventListener('mouseover', _ => {
   btn[2].style.backgroundColor = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
 });
+
+//Text appears when double clicking on paragraph
+
+const allPara = document.querySelectorAll('p');
+
+allPara[1].addEventListener('dblclick', e => {
+  allPara[1].textContent += ' OUCH! STOP POKING ME '
+});
